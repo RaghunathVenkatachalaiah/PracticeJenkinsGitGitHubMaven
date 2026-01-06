ba05e34e6e5203ac8178b20d4bf9de808ddb8e6a
RaghunathVenkatachalaiah <raghunath.v28@gmail.com>
2026-01-06 12:54:56 +0530

    2nd commit

---
+++ src/test/java/PracticeJenkinsGitHubCommands/Test2.java
@@ -0,0 +1,14 @@
+package PracticeJenkinsGitHubCommands;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	
+	@Test
+	public void Test2() {
+		
+		System.out.println("Test2");
+	}
+
+}
